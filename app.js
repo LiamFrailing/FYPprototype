d1e54039ef7995d91a59507e8c958958ce3b4240
LiamFrailing <frailinl@aston.ac.uk>
2019-01-11 14:25:48 +0000

    Gender classification v1

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ To run application:
 */
 
 // Node modules required for app
+var csvtojson = require("csvtojson");
 var Sentiment = require('./node_modules/sentiment');
 var twit = require('./node_modules/twit');
 var config = require('./config');
@@ -12,7 +13,76 @@ var config = require('./config');
 // Setup twitter connection details
 var T = new twit(config);
 
-getTweets();
+getProfile();
+
+
+// Retrieve the given users Twitter profile
+function getProfile(){
+
+    var params = {
+        screen_name: 'dapperlaughs'
+    }
+
+    var txt = '';
+
+    T.get('users/show', params, gotData);
+    function gotData(err, data, response){
+        
+        var fullName = data.name.trim();
+        var firstName = fullName.split(' ')[0].toLowerCase().trim();
+        var results = [];
+
+        const csvFilePath = 'data/gender_labelled_names.csv';
+
+        csvtojson()
+            .fromFile(csvFilePath)
+            .then((jsonObj)=>{
+                    for (var i = 0; i < jsonObj.length; i++) {
+                        if(jsonObj[i].Name.toLowerCase() == firstName){
+                            console.log(jsonObj[i]);
+                            results.push(jsonObj[i]);
+                        }
+                    }
+                    if(results.length == 1){
+                        if(results[0].Quantity >= 50){
+                            console.log("Likely gender for this name: " + results[0].Gender);
+                        }
+                        else{
+                            // correlation not strong enough... cannot classify
+                        }
+                    }
+                    else if(results.length == 2){
+                        if(results[0].Quantity >= 100 || results[1].Quantity >= 100){
+                            var first = parseInt(results[0].Quantity);
+                            var second = parseInt(results[1].Quantity);
+                            var dif = first - second;
+
+                            // make negative difference a positive int
+                            if (dif < 0) {
+                                dif = dif + (2 * -dif);
+                            }
+
+                            // check difference greater than 70% relative to total quantity
+                            if ( dif / (first + second ) > 0.7) { 
+                                if (first > second){
+                                    console.log("Likely gender for this name: " + results[0].Gender);
+                                }
+                                else {
+                                    console.log("Likely gender for this name: " + results[1].Gender);
+                                }
+                            }
+                            else {
+                                // skew not strong enough... cannot classify
+                            }
+                        }
+                    }
+                    else {
+                        // no matches found in csv
+                        console.log("No names match the input");
+                    }
+            })
+    }
+}
 
 
 // Retrieve tweets according to search parameters
@@ -34,6 +104,7 @@ function getTweets(){
         runSentiment(txt);
     }
 }
+
 
 /*
 // Function to run sentiment analysis on a given string
