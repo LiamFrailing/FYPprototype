17b1f84836813b56b150582d5744113e8ce0d6ae
LiamFrailing <frailinl@aston.ac.uk>
2019-02-04 11:56:07 +0000

    Personality Profiling V0.1

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var config = require('./config');
 
 // Setup twitter connection details
 var T = new twit(config);
-var username = 'dapperlaughs';
+var username = 'chayseimoni';
 
 getGender(username);
 getPersonalityProfile(username);
@@ -103,13 +103,14 @@ function getPersonalityProfile(username){
             txt += data[i].text;           
         }
         
+        var totalWords = txt.split(' ').length;
         var sentiment = new Sentiment();
         var result = sentiment.analyze(txt);
         var tonals = result.positive.concat(result.negative);
         const csvFilePath = 'data/NRC-Emotion-Lexicon.csv';
         
         // emotions map for counting the number of time each emotion occurs in the tweets
-        var emotions = { 'anger': 0, 'anticipation': 0, 'disgust': 0, 'fear': 0, 'joy': 0, 'sadness': 0, 'surprise': 0, 'trust': 0};
+        var emotions = { 'positive': result.positive.length, 'negative': result.negative.length, 'anger': 0, 'anticipation': 0, 'disgust': 0, 'fear': 0, 'joy': 0, 'sadness': 0, 'surprise': 0, 'trust': 0};
 
         csvtojson()
         .fromFile(csvFilePath)
@@ -124,11 +125,62 @@ function getPersonalityProfile(username){
                             emotions.joy += parseInt(jsonObj[x].Joy);
                             emotions.sadness += parseInt(jsonObj[x].Sadness);
                             emotions.surprise += parseInt(jsonObj[x].Surprise);
-                            emotions.trust += parseInt(jsonObj[x].Trust);
+                            emotions.trust += parseInt(jsonObj[x].Trust);                     
                         }
                 }
             }
+            console.log(totalWords);
+            console.log(tonals.length);
             console.log(emotions);
+            console.log("Dominance: " + (tonals.length / totalWords) + " and " + Math.abs(emotions.positive - emotions.negative) /  (emotions.positive + emotions.negative));
+            console.log("Influence: " + emotions.trust / tonals.length + " and " + emotions.anticipation / tonals.length + " and " + emotions.joy / tonals.length);
+            console.log("Steadiness: " + (tonals.length / totalWords) + " and " + emotions.joy / tonals.length);
+            console.log("Compliance: " + emotions.trust / tonals.length + " and " + emotions.fear / tonals.length);
+
+            console.log(tonals.length / totalWords);
+            console.log(Math.abs(emotions.positive - emotions.negative) /  (emotions.positive + emotions.negative));
+            console.log();
+            console.log(emotions.trust / tonals.length);
+            console.log(emotions.anticipation / tonals.length);
+            console.log(emotions.joy / tonals.length);
+            console.log();
+            console.log(tonals.length / totalWords);
+            console.log(emotions.joy / tonals.length);
+            console.log();
+            console.log(emotions.trust / tonals.length);
+            console.log(emotions.fear / tonals.length);
+
+            /* conditions for DOMINANCE: 
+                - High % of emotive words relative to total word count
+                - High % of emotional mix (i.e. similarly high pos/neg count) */
+            if((tonals.length / totalWords) > 0.08 && 
+                Math.abs(emotions.positive - emotions.negative) / (emotions.positive + emotions.negative) < 0.16){
+                console.log("Dominance: YES");
+            } else { console.log("Dominance: NO"); }
+
+            /* conditions for INFLUENCE: 
+                - High % of trust words relative to tonal word count
+                - High % of anticipation words relative to tonal word count 
+                - High % of joy words relative to tonal word count */
+            if(emotions.trust / tonals.length > 0.13 && emotions.anticipation / tonals.length > 0.11 && emotions.joy / tonals.length > 0.16){
+                console.log("Influence: YES");
+            } else { console.log("Influence: NO"); }
+
+            /* conditions for STEADINESS: 
+                - Low % of emotive words relative to total word count
+                - High % of joy relative to tonal word count */
+                if((tonals.length / totalWords) < 0.08 && 
+                (emotions.joy / tonals.length > 0.16)){
+                console.log("Steadiness: YES");
+            } else { console.log("Steadiness: NO"); }
+
+            /* conditions for COMPLIANCE: 
+                - Low % of trust words relative to tonal word count
+                - High % of fear words relative to tonal word count */
+            if(emotions.trust / tonals.length < 0.13 && emotions.fear / tonals.length > 0.1){
+                console.log("Compliance: YES");
+            } else { console.log("Compliance: NO"); }
+            /* No personality profile identified */
         })
     }
 
