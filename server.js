df7b22e58a779cdb2e67bffcbc0d02b655f634cc
LiamFrailing <frailinl@aston.ac.uk>
2019-03-04 10:22:25 +0000

    Prototype working version 1.0

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
+/* 
+To run application: 
+    cd  Documents/'ASTON YEAR 3'/'Individual Project'/Prototype
+    node server.js
+*/
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
@@ -23,6 +29,7 @@ app.get('/', function (req, res) {
 app.post('/', function (req, res) { 
   getGender(req.body.username)
   getAgeRange(req.body.username)
+  getPersonalityProfile(req.body.username)
 
   setTimeout(function () {
     if(fs.existsSync('profile.txt')){
@@ -30,8 +37,10 @@ app.post('/', function (req, res) {
         if (err) throw err;
 
         var results = data.toString().split(',');
-
-        res.render('results', {username:req.body.username, gender: results[0], age: results[1]});
+        if(!results.length < 6){
+            res.render('results', {username:req.body.username, gender: results[0], age: results[1], dominance: results[2], 
+            influence: results[3], steadiness: results[4], compliance: results[5]});
+        }
       });
     }
   }, 1000);
@@ -59,7 +68,7 @@ function getGender(username){
       var firstName = fullName.split(' ')[0].toLowerCase().trim();
       var results = [];
 
-      var gender = '';
+      var gender = "Gender: ";
 
       const csvFilePath = 'data/gender_labelled_names.csv';
       
@@ -74,7 +83,7 @@ function getGender(username){
                   }
                   if(results.length == 1){
                       if(results[0].Quantity >= 50){
-                          gender = results[0].Gender + ", ";
+                          gender = "Gender: " + results[0].Gender + ",";
                       }
                       else{
                           // correlation not strong enough... cannot classify
@@ -94,10 +103,10 @@ function getGender(username){
                           // check difference greater than 70% relative to total quantity
                           if ( dif / (first + second ) > 0.7) { 
                               if (first > second){
-                                  gender = results[0].Gender + ", ";
+                                  gender = "Gender: " + results[0].Gender + ",";
                               }
                               else {
-                                  gender = results[1].Gender + ", ";
+                                  gender = "Gender: " + results[1].Gender + ",";
                               }
                           }
                           else {
@@ -107,7 +116,7 @@ function getGender(username){
                   }
                   else {
                       // no matches found in csv
-                      gender = "Gender undetermined, ";
+                      gender = "Gender: undetermined,";
                   }
                   // write to a new file named gender.txt
                   fs.writeFile('profile.txt', gender, (err) => {  
@@ -162,15 +171,204 @@ function getAgeRange(username){
       const age = predictAge(txt, opts)
 
       if(age.AGE < 30){
-          ageRange = "\r\nAge range: 16-30";
-      } else { ageRange = "\r\nAge range: 30+"; }
+          ageRange = "Age: 16-30,";
+      } else { ageRange = "Age: 30+,"; }
 
-      fs.appendFile('profile.txt', ageRange, (err) => {  
+      fs.appendFileSync('profile.txt', ageRange, (err) => {  
         // throws an error, you could also catch it here
         if (err) throw err;
 
         // success case, the file was saved
         console.log('Age saved!');
       });
+  }
+}
+
+/**********************************************************************
+ * Infer personality profile from the given username
+ **********************************************************************/
+
+ // Determine the personality profile of the given user
+function getPersonalityProfile(username){
+
+  var params = {
+      screen_name: username,
+      count: 3200,
+      include_rts: false
+  }
+
+  var txt = '';
+
+  T.get('statuses/user_timeline', params, gotData);
+
+  function gotData(err, data, response){
+      
+      for (var i = 0; i < data.length; i++) {
+          txt += data[i].text;           
+      }
+      
+      var totalWords = txt.split(' ').length;
+      var sentiment = new Sentiment();
+      var result = sentiment.analyze(txt);
+      var tonals = result.positive.concat(result.negative);
+      const csvFilePath = 'data/NRC-Emotion-Lexicon.csv';
+      
+      // emotions map for counting the number of time each emotion occurs in the tweets
+      var emotions = { 'positive': result.positive.length, 'negative': result.negative.length, 'anger': 0, 'anticipation': 0, 'disgust': 0, 'fear': 0, 'joy': 0, 'sadness': 0, 'surprise': 0, 'trust': 0};
+
+      csvtojson()
+      .fromFile(csvFilePath)
+      .then((jsonObj)=>{
+          for (var i = 0; i < tonals.length; i++) {
+              for (var x = 0; x < jsonObj.length; x++) {
+                      if(tonals[i].toLowerCase() == jsonObj[x].Word){
+                          emotions.anger += parseInt(jsonObj[x].Anger);
+                          emotions.anticipation += parseInt(jsonObj[x].Anticipation);
+                          emotions.disgust += parseInt(jsonObj[x].Disgust);
+                          emotions.fear += parseInt(jsonObj[x].Fear);
+                          emotions.joy += parseInt(jsonObj[x].Joy);
+                          emotions.sadness += parseInt(jsonObj[x].Sadness);
+                          emotions.surprise += parseInt(jsonObj[x].Surprise);
+                          emotions.trust += parseInt(jsonObj[x].Trust);                     
+                      }
+              }
+          }
+
+          var numMentions = 0;
+          for (var i = 0; i < txt.length; i++) {
+              if (txt.charAt(i) == "@") {
+                 numMentions ++;
+              }
+          }
+
+          var dominanceCounter = 0;
+          var steadinessCounter = 0;
+
+          // Dominance OR Steadiness skew analysis - high emotion
+          var emotivePercent = (tonals.length / totalWords) * 100;
+          if( (Math.round(emotivePercent - 8)) >= 0){
+              dominanceCounter++;
+          } else { 
+              steadinessCounter++; }
+          
+          // Dominance OR Steadiness skew analysis - high mention count
+          var numMentions = 0;
+          for (var i = 0; i < txt.length; i++) {
+              if (txt.charAt(i) == "@") {
+                 numMentions ++;
+              }
+          }
+          var mentionPercent = (numMentions / totalWords) * 100;
+          if( (Math.round(mentionPercent - 10)) >= 0){
+              dominanceCounter++;
+          } else { 
+              steadinessCounter++; }
+          
+          // Dominance OR Steadiness skew analysis - high anger
+          var angerPercent = (emotions.anger / tonals.length) * 100;
+          if( (Math.round(angerPercent - 10)) >= 0){
+              dominanceCounter++;
+          } else { 
+              steadinessCounter++; }
+          
+          // Dominance OR Steadiness skew analysis - high surprise
+          var surprisePercent = (emotions.surprise / tonals.length) * 100;
+          if( (Math.round(surprisePercent - 8)) >= 0){
+              dominanceCounter++;
+          } else { 
+              steadinessCounter++; }
+
+          // Dominance OR Steadiness skew analysis - low fear
+          var fearPercent = (emotions.fear / tonals.length) * 100;
+          if( (Math.round(fearPercent - 10)) <= 0){
+              dominanceCounter++;
+          } else { 
+              steadinessCounter++; }
+
+          // Dominance OR Steadiness skew analysis - low trust
+          var trustPercent = (emotions.trust / tonals.length) * 100;
+          if( (Math.round(trustPercent - 17)) <= 0){
+              dominanceCounter++;
+          } else { 
+              steadinessCounter++; }
+          
+          //console.log('Dominance: ' + dominanceCounter + ' && ' + 'Steadiness: ' + steadinessCounter);
+
+          var influenceCounter = 0;
+          var complianceCounter = 0;
+
+          // Influence OR Compliance skew analysis - high positivity
+          var positivityPercent = (emotions.positive / tonals.length) * 100;
+          if( Math.round((positivityPercent - 66)) >= 0){
+              influenceCounter++;
+          } else { 
+              complianceCounter++; }
+
+          // Influence OR Compliance skew analysis - high mention count
+          if( (Math.round(mentionPercent - 10)) >= 0){
+              influenceCounter++;
+          } else { 
+              complianceCounter++; }
+
+          // Influence OR Compliance skew analysis - high anticipation
+          var anticipationPercent = (emotions.anticipation / tonals.length) * 100;
+          if( Math.round((anticipationPercent - 16)) >= 0){
+              influenceCounter++;
+          } else { 
+              complianceCounter++; }
+          
+          // Influence OR Compliance skew analysis - high joy
+          var joyPercent = (emotions.joy / tonals.length) * 100;
+          if( Math.round((joyPercent - 20)) >= 0){
+              influenceCounter++;
+          } else { 
+              complianceCounter++; }
+          
+          // Influence OR Compliance skew analysis - high trust
+          var trustPercent = (emotions.trust / tonals.length) * 100;
+          if( Math.round((trustPercent - 17)) >= 0){
+              influenceCounter++;
+          } else { 
+              complianceCounter++; }
+
+          // Influence OR Compliance skew analysis - low fear
+          var fearPercent = (emotions.fear / tonals.length) * 100;
+          if( Math.round((fearPercent - 10)) <= 0){
+              influenceCounter++;
+          } else { 
+              complianceCounter++; }
+
+          fs.appendFileSync('profile.txt', "Dominance: " + Math.round((dominanceCounter / 12) * 100) + "%,", (err) => {  
+            // throws an error, you could also catch it here
+            if (err) throw err;
+    
+            // success case, the file was saved
+            console.log('Dominance score saved!');
+          });
+
+          fs.appendFileSync('profile.txt', "Influence: " + Math.round((influenceCounter / 12) * 100) + "%,", (err) => {  
+            // throws an error, you could also catch it here
+            if (err) throw err;
+    
+            // success case, the file was saved
+            console.log('Influence score saved!');
+          });
+
+          fs.appendFileSync('profile.txt', "Steadiness: " + Math.round((steadinessCounter / 12) * 100) + "%,", (err) => {  
+            // throws an error, you could also catch it here
+            if (err) throw err;
+    
+            // success case, the file was saved
+            console.log('Steadiness score saved!');
+          });
+
+          fs.appendFileSync('profile.txt', "Compliance: " + Math.round((complianceCounter / 12) * 100) + "%", (err) => {  
+            // throws an error, you could also catch it here
+            if (err) throw err;
+    
+            // success case, the file was saved
+            console.log('Compliance score saved!');
+          });
+      })
   }
 }
